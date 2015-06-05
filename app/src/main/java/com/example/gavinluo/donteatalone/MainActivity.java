35614bcfd0077f198d3fa7570c8de656cb6ebef3
zjcai <z24cai.waterloo@gmail.com>
2015-06-05 00:12:41 -0400

    First commit

--- app/src/main/java/com/example/gavinluo/donteatalone/MainActivity.java
+++ app/src/main/java/com/example/gavinluo/donteatalone/MainActivity.java
@@ -1,18 +1,65 @@
 package com.example.gavinluo.donteatalone;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.facebook.*;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_main);
+
+        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        //TextView info = (TextView)findViewById(R.id.info);
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                TextView info = (TextView)findViewById(R.id.info);
+                info.setText("User ID: " + loginResult.getAccessToken().getUserId() + "\n" +
+                                "AuthToken: " + loginResult.getAccessToken().getToken() + "\n" +
+                                    "ToString: " + loginResult.getAccessToken().toString());
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                TextView info = (TextView)findViewById(R.id.info);
+                info.setText("Login attempt canceled");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                TextView info = (TextView)findViewById(R.id.info);
+                info.setText("Login attempt failed" + exception.toString());
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
