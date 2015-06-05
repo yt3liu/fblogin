ab2afb94d01096c47d30ac6794c4e458e5f6f908
Joyce Mac <yt3liu@uwaterloo.ca>
2015-06-05 11:47:17 -0400

    remove unused code

--- app/src/main/java/com/example/gavinluo/donteatalone/MainActivity.java
+++ app/src/main/java/com/example/gavinluo/donteatalone/MainActivity.java
@@ -27,31 +27,6 @@ public class MainActivity extends ActionBarActivity {
 
     CallbackManager callbackManager;
 
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,7 +37,6 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
-        //TextView info = (TextView)findViewById(R.id.info);
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -74,14 +48,6 @@ public class MainActivity extends ActionBarActivity {
                                     "ToString: " + loginResult.getAccessToken().toString());
 
                 Profile profile = Profile.getCurrentProfile();
-                String profileInfo = profile.toString();
-//                info.setText(profileInfo);
-
-                ImageView image = (ImageView)findViewById(R.id.image);
-//                image.setImageURI(profile.getLinkUri());
-
-//                new DownloadImageTask((ImageView) findViewById(R.id.image))
-//                        .execute("http://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png");
 
                 String output = "";
                 output = "ID: " + profile.getId() +"\n";
@@ -92,23 +58,9 @@ public class MainActivity extends ActionBarActivity {
 
                 info.setText(output);
 
-//                Uri uri = Uri.parse(profile.getLinkUri().toString());
-//                Uri uri = Uri.parse("https://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png");
-//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-//                startActivity(intent);
-
                 ProfilePictureView profilePictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
                 profilePictureView.setProfileId(profile.getId());
 
-                Log.i("PICTURE", profile.getLinkUri().toString());
-
-//                info.setText(Html.fromHtml("<a href=http://www.stackoverflow.com> STACK OVERFLOW "));
-//                info.setText(Html.fromHtml("<a href=" + profile.getLinkUri() +"> STACK OVERFLOW "));
-//                info.setMovementMethod(LinkMovementMethod.getInstance());
-
-//                ImageView image = (ImageView)findViewById(R.id.image);
-//                image.setImageURI(profile.getLinkUri());
-//                image.setImageURI("https://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png");
             }
 
             @Override
