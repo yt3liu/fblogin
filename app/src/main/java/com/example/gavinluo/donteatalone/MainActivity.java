5c5c955ef32446f0b7bc2e7d9e51ec8c8664f1a3
Joyce Mac <yt3liu@uwaterloo.ca>
2015-06-05 11:41:19 -0400

    Added Profile Image

--- app/src/main/java/com/example/gavinluo/donteatalone/MainActivity.java
+++ app/src/main/java/com/example/gavinluo/donteatalone/MainActivity.java
@@ -1,20 +1,56 @@
 package com.example.gavinluo.donteatalone;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.util.Log;
 
 import com.facebook.*;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import java.io.InputStream;
 
 
 public class MainActivity extends ActionBarActivity {
 
     CallbackManager callbackManager;
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +70,45 @@ public class MainActivity extends ActionBarActivity {
                 // App code
                 TextView info = (TextView)findViewById(R.id.info);
                 info.setText("User ID: " + loginResult.getAccessToken().getUserId() + "\n" +
-                                "AuthToken: " + loginResult.getAccessToken().getToken() + "\n" +
+                        "AuthToken: " + loginResult.getAccessToken().getToken() + "\n" +
                                     "ToString: " + loginResult.getAccessToken().toString());
+
+                Profile profile = Profile.getCurrentProfile();
+                String profileInfo = profile.toString();
+//                info.setText(profileInfo);
+
+                ImageView image = (ImageView)findViewById(R.id.image);
+//                image.setImageURI(profile.getLinkUri());
+
+//                new DownloadImageTask((ImageView) findViewById(R.id.image))
+//                        .execute("http://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png");
+
+                String output = "";
+                output = "ID: " + profile.getId() +"\n";
+                output += "FirstName: " + profile.getFirstName() + "\n";
+                output += "MiddleName: " + profile.getMiddleName() + "\n";
+                output += "LastName: " + profile.getLastName() + "\n";
+                output += "ProfilePic: " + profile.getProfilePictureUri(100, 100) + "\n";
+
+                info.setText(output);
+
+//                Uri uri = Uri.parse(profile.getLinkUri().toString());
+//                Uri uri = Uri.parse("https://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png");
+//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//                startActivity(intent);
+
+                ProfilePictureView profilePictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
+                profilePictureView.setProfileId(profile.getId());
+
+                Log.i("PICTURE", profile.getLinkUri().toString());
+
+//                info.setText(Html.fromHtml("<a href=http://www.stackoverflow.com> STACK OVERFLOW "));
+//                info.setText(Html.fromHtml("<a href=" + profile.getLinkUri() +"> STACK OVERFLOW "));
+//                info.setMovementMethod(LinkMovementMethod.getInstance());
+
+//                ImageView image = (ImageView)findViewById(R.id.image);
+//                image.setImageURI(profile.getLinkUri());
+//                image.setImageURI("https://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png");
             }
 
             @Override
